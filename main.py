a4f7e4a739e02de763aa8e649fd276cbc3dbd54c
melvintu <melvin@meldano.com>
2025-10-13 22:11:43 +1100

    Added in player rotation and movement. Added in player grouping containers

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import pygame
 from constants import *
 from player import *
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0 # delta time
 
     running = True
@@ -15,8 +23,13 @@ def main():
             if event.type == pygame.QUIT:
                 return # return just exits the function main()
             
-        pygame.Surface.fill(screen, (0,0,0)) # fills screen variable, then 0,0,0 is the colour
-        player.draw(screen)
+        updatable.update(dt)
+        
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 

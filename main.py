dd6bcf68409c5f69c9849511f919eb3c476b2b93
melvintu <melvin@meldano.com>
2025-10-13 20:31:10 +1100

    Added while loop for black screen

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return # return just exits the function main()
+        pygame.Surface.fill(screen, (0,0,0)) # fills screen variable, then 0,0,0 is the colour
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
